2ae67733acfcb3679e6a3f18baa719f539c0573e
Vítor Brandão <noisebleed@noiselabs.org>
2013-01-05 17:56:11 +0000

    Add Module.php

---
+++ Module.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of NoiselabsDistributionBundle
+ *
+ * NoiselabsDistributionBundle is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NoiselabsDistributionBundle is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NoiselabsDistributionBundle; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2013 Vítor Brandão
+ *
+ * @category    NoiseLabs
+ * @package     DistributionBundle
+ * @copyright   (C) 2013 Vítor Brandão <noisebleed@noiselabs.org>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL-3
+ * @link        http://www.noiselabs.org
+ */
+
+namespace NoiseLabs\Module\DistributionModule;
+
+use PPI\Module\AbstractModule;
+
+/**
+ * PPI Framework DistributionModule.
+ *
+ * @author Vítor Brandão <noisebleed@noiselabs.org>
+ */
+class Module extends AbstractModule
+{
+    const VERSION = '0.0.1-DEV';
+
+    protected $_moduleName = 'NoiselabsDistributionBundle';
+
+    public function init($e)
+    {
+        Autoload::add(__NAMESPACE__, dirname(__DIR__));
+    }
+}
